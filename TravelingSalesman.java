a7875d96e5ada58f43f5f47966730e4df97ee560
Alpq <doymazalp@gmail.com>
2021-01-20 17:16:44 -0500

    Updated Java file with possible steps for problem. Feel free to edit these.

---
+++ TravelingSalesman.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class TravelingSalesman{
+  public static void main(String[] args) {
+    /*
+    1. Scan the file and find # of cities / distances between cities.
+    2. Create a 2d array with #cities x #cities dimensions.
+    3. Assign each city a numerical value from 0->(# of cities).
+    4. Based on the number of cities find the # of permutations needed to find the shortest distance.
+    Loop x times{
+    5. Shuffle a List with ints from 0->(# of cities).
+    6. Iterate through list and find distances between each of the number.
+    7. Check if this iteration has the shortest total distance (if so update new shortest distance). 
+    }
+    8. Output shortest distance.
+    */
+  }
