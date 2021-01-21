85e4a62dfab136114688d1493722312e8a8678da
Alpq <doymazalp@gmail.com>
2021-01-20 20:06:48 -0500

    Removed cities2 array so that all cities were in a single array and distances[][] could be filled.

--- TravelingSalesman.java
+++ TravelingSalesman.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class TravelingSalesman{
   public static void main(String[] args){
     ArrayList<String> cities = new ArrayList<String>(); //arraylist for starting cities
-    ArrayList<String> cities2 = new ArrayList<String>();//arraylist for destination cities
     int distances[][] = new int[9][9]; //is there an upper limit that we need to test?
     Scanner n = new Scanner(System.in);
     while (n.hasNext()){
@@ -15,19 +14,14 @@ public class TravelingSalesman{
 
       linescan.next(); //skips "to"
       String destinationC = linescan.next();
-      if (cities2.indexOf(destinationC) == -1)
-        cities2.add(destinationC); //if cannot find city in 'cities2'(returns -1), add to list
+      if (cities.indexOf(destinationC) == -1)
+        cities.add(destinationC); //if cannot find city in 'cities2'(returns -1), add to list
 
       linescan.next(); //skips "="
       int distance = linescan.nextInt(); //finds distance
-      distances[cities.indexOf(startingC)][cities2.indexOf(destinationC)] = distance;
-      distances[cities2.indexOf(destinationC)][cities.indexOf(startingC)] = distance;
+      distances[cities.indexOf(startingC)][cities.indexOf(destinationC)] = distance;
+      distances[cities.indexOf(destinationC)][cities.indexOf(startingC)] = distance;
     }
-/*    System.out.println(cities.toString());
-    System.out.println(cities2.toString());*/
-    //we can probably fit startingC and destinationC in the same 'cities' arraylist,
-    //but idk if that messes up the order of the starting cities and such
-
     int shortestPath = Integer.MAX_VALUE;
     ArrayList<Integer> integerPaths = new ArrayList<Integer>();
     for (int i = 0; i < cities.size() ; i++) {integerPaths.add(i);}
